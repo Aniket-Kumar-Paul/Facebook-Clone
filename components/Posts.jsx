439308183a9b01576156c8d3b28953f8b24384cb
Aniket <aniketkumarpaul1@gmail.com>
2022-07-25 17:17:07 +0530

    posts section

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { useCollection } from "react-firebase-hooks/firestore"
+import { db } from "../firebase"
+import Post from './Post';
 
 function Posts() {
-  return (
-    <div>
-        
-    </div>
-  )
+    const [realtimePosts, loadingState, error] = useCollection(
+        db.collection('posts').orderBy('timestamp', 'desc') // descending
+    );
+
+    return (
+        <div>
+            {realtimePosts?.docs.map(post => (
+                <Post
+                    key={post.id}
+                    name={post.data().message}
+                    email={post.data().email}
+                    timestamp={post.data().timestamp}
+                    image={post.data().image}
+                    postImage={post.data().postImage}
+                />
+            ))}
+        </div>
+    )
 }
 
 export default Posts
