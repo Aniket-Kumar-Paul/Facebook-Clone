241869cec53acc78edd0496d5ebed0cdb0ecc3b2
Aniket <aniketkumarpaul1@gmail.com>
2022-07-25 17:50:15 +0530

    added widgets

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,15 +5,12 @@ import Stories from './Stories'
 
 function Feed() {
   return (
-    <div className='flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto'>
-        <div className='mx-auto max-w-md md:max-w-lg'>
-           {/* stories */}
-           <Stories />
-           {/* inputbox */}
-           <InputBox />
-           {/* posts */}
-           <Posts />
-        </div>
+    <div className='flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto scrollbar-hide'>
+      <div className='mx-auto max-w-md md:max-w-lg'>
+        <Stories />
+        <InputBox />
+        <Posts />
+      </div>
     </div>
   )
 }
